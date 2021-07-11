9ea191f4fd64806006c3e4eacb5f0556fe969eb1
chanhyun <stephano2037@gmail.com>
2021-07-11 13:26:12 +0900

    re #2 time-7m cycle of add

--- Algorithm/Algorithm_group/Algorithm_group/main.cpp
+++ Algorithm/Algorithm_group/Algorithm_group/main.cpp
@@ -8,19 +8,17 @@ typedef union {
 	int iNum2;
 }union_Number;
 
-typedef struct {
-	int m_iNum;
-	int m_iNum2;
-}tNumber;
+
+void checkRightSide(int num)
+{
+
+}
 
 
 int main()
 {
-
+	/* 21.07.11 while 문 기초 알고리즘
 	tNumber Number = {0,};
-
-	
-	
 	while (1)
 	{
 		//cin >> Number.m_iNum >> Number.m_iNum2;
@@ -30,8 +28,44 @@ int main()
 
 		printf(" %d\r\n", iSum);
 	}
+	*/
+	
+	
+	/*  210711 cycle of add
+	*/
+	
+	int iNum = 0;
+	int iCycleCount = 0;
+	int iSum = 0;
 
-	//uNumber.iNum = 1;
+	//under 100
+	while (1)
+	{
+		scanf_s("%d", &iNum);
+
+		if (iNum < 0 || iNum>99) continue;
+		else break;
+	}
 	
 
-}
+	iSum = iNum;
+	//10으로 나누는것은 동일
+	while (1)
+	{
+		int iTemp = (iSum / 10);
+		int iReverse = (iSum % 10);
+		int iTemp_sum = iTemp + iReverse;
+		int iTemp_S_Reverse = (iTemp_sum % 10);
+
+		iSum = (iReverse * 10) + iTemp_S_Reverse;
+
+		++iCycleCount;
+
+		if (iSum == iNum)
+			break;
+		
+	}
+
+	printf("%d\r\n", iCycleCount);
+
+}//end of while

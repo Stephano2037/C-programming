3262bb419d4e25feb5797f76dfced737b969bed8
chanhyun <stephano2037@gmail.com>
2021-07-11 15:39:27 +0900

    re #3 time -20m arithmetic sequence algorithm
    - https://www.acmicpc.net/problem/1065
    
    - input  condition
    -- (n<=1000)

--- Algorithm/Algorithm_group/Algorithm_group/main.cpp
+++ Algorithm/Algorithm_group/Algorithm_group/main.cpp
@@ -9,10 +9,32 @@ typedef union {
 }union_Number;
 
 
-void checkRightSide(int num)
+int checkSameDelta(int num)
 {
+	int iNum = 0;
+	int iCount = 0;
+	int iDecimal = 100;
 
-}
+	int iQuotientArray[100] = { 0, };
+	int iReverse[100] = { 0, };
+
+	while (iDecimal!=1){
+		iQuotientArray[iCount] = num / (iDecimal);
+		iReverse[iCount] = num % iDecimal;
+
+		num = iReverse[iCount++];
+		iDecimal /= 10;
+		
+	}//end of inner while
+
+	if ( iQuotientArray[iCount - 1]- iQuotientArray[iCount - 2]  ==  (iReverse[iCount - 1]) - iQuotientArray[iCount - 1]  )
+	{
+		iNum = 1;
+	}
+
+
+	return iNum;
+}//end of checkSameDelta
 
 
 int main()
@@ -32,8 +54,6 @@ int main()
 	
 	
 	/*  210711 cycle of add
-	*/
-	
 	int iNum = 0;
 	int iCycleCount = 0;
 	int iSum = 0;
@@ -41,31 +61,79 @@ int main()
 	//under 100
 	while (1)
 	{
-		scanf_s("%d", &iNum);
+	scanf_s("%d", &iNum);
 
-		if (iNum < 0 || iNum>99) continue;
-		else break;
+	if (iNum < 0 || iNum>99) continue;
+	else break;
 	}
-	
+
 
 	iSum = iNum;
 	//10으로 나누는것은 동일
 	while (1)
 	{
-		int iTemp = (iSum / 10);
-		int iReverse = (iSum % 10);
-		int iTemp_sum = iTemp + iReverse;
-		int iTemp_S_Reverse = (iTemp_sum % 10);
+	int iTemp = (iSum / 10);
+	int iReverse = (iSum % 10);
+	int iTemp_sum = iTemp + iReverse;
+	int iTemp_S_Reverse = (iTemp_sum % 10);
 
-		iSum = (iReverse * 10) + iTemp_S_Reverse;
+	iSum = (iReverse * 10) + iTemp_S_Reverse;
 
-		++iCycleCount;
+	++iCycleCount;
 
-		if (iSum == iNum)
-			break;
-		
+	if (iSum == iNum)
+	break;
+
 	}
 
 	printf("%d\r\n", iCycleCount);
+	*/
+	
+	/*210711 cycle of add
+	*/
+	volatile bool bKeepgoing = true;
+	int iNum = 0;
+	int iCount = 0;
+	int iRv = 0;
+
+
+
+	while (bKeepgoing)
+	{
+		scanf_s("%d", &iNum);
+		switch (iNum / 100) {
+		case 0: {
+			iCount = 0;
+			printf("%d\r\n", iNum + iCount);
+			bKeepgoing = false;
+			break;
+		}
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		{
+			iCount += 99;
+			bKeepgoing = false;
+
+			for (int i = 100; i <= iNum; ++i)
+			{
+				iRv += checkSameDelta(i);
+			//	if (iRv != 0)
+				//	printf("[%d] \r\n" ,i );
+			}
+				
+
+
+			printf("%d\r\n", iRv + iCount);
+			break;
+		}
+		}//end of switch
+	}//end of while
 
 }//end of while
